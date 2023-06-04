dd0dbf0cdc011014675b51424787ad452ed66393
vanluhurla <94018863+vanluhurla@users.noreply.github.com>
2023-06-04 16:03:14 +0100

    Add colour palette

---
+++ GoodChef/Design/GCColours.swift
@@ -0,0 +1,16 @@
+//
+//  GCColours.swift
+//  GoodChef
+//
+//  Created by Vanessa Hurla on 04/06/2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static let backgroundPrimary = UIColor(named: "backgroundPrimary")
+    static let backgroundSecondary = UIColor(named: "backgroundSecondary")
+    static let accent = UIColor(named: "accent")
+    static let primaryText = UIColor(named: "primaryText")
+    static let secondaryText = UIColor(named: "secondaryText")
+}
